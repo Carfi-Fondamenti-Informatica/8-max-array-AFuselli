aeee8bd2a92ea4bd5dcb898dfa79edb6b7092e70
AFuselli <91535784+AFuselli@users.noreply.github.com>
2021-12-09 19:14:22 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,1 +1,11 @@
+//
+// Created by Andrea Fuselli on 09/12/21.
+//
+
+#ifndef COMPITO8_MAXARRAY_LIB_H
+#define COMPITO8_MAXARRAY_LIB_H
+
+float funzione(float[], int);
+
+#endif //COMPITO8_MAXARRAY_LIB_H
 
