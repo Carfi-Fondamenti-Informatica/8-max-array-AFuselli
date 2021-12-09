3909bec3ec6843460e7b6ad049a62fa324c566e6
AFuselli <91535784+AFuselli@users.noreply.github.com>
2021-12-09 19:15:04 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+    int dim=0;
+    cin >> dim;
+    float a[dim];
+    for(int i=0; i < dim; i++){
+        cin >> a[i];
+    }
+    cout << funzione(a, dim);
+    return 0;
 }
